82ac197c92a1c816ce859d3bceae806b23380bf7
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2021-02-07 18:03:55 +0300

    Create main_cpu.cpp

---
+++ CPU/main_cpu.cpp
@@ -0,0 +1,33 @@
+#include "cpu.h"
+
+int main()
+{
+    printf("CPU..\n");
+
+    setlocale(LC_ALL, "russian");
+
+    FILE* text = fopen("[!]assembler_code.txt", "r");
+    FILE* res  = fopen("[!]log_stack.txt", "ab");
+
+    fprintf(res, "\tДата log_stack'a : %s (чч/мм/гг)\n\n", define_date());
+    fclose(res);
+
+    char* name = nullptr;
+    START_ACTIONS(Stack);
+    Construct(&Stack, START_SIZE);
+
+    struct ass_code ass_s = {};
+    make_ass_s(text, &ass_s);
+
+
+    CPU(&ass_s, &Stack);     //(&ass_s, &Stack, &reg_s); //with registers
+
+    print_for_user(&Stack);  // information for user
+    stack_dump(&Stack);      // information for me
+    printf("DONE!!\n");
+    //getch();
+
+    ass_code_destruct(&ass_s);
+
+    return 0;
+}
