02f215e9c13d7b26b44317447007499a74af40ca
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2021-02-07 18:04:51 +0300

    Create cpu_info.h

---
+++ CPU/cpu_info.h
@@ -0,0 +1,12 @@
+#ifndef CPU_INFO_H_INCLUDED
+#define CPU_INFO_H_INCLUDED
+
+const int START_SIZE = 10;
+
+struct ass_code
+{
+    double* data            = nullptr;
+    int     ass_size        = 0;
+};
+
+#endif // CPU_INFO_H_INCLUDED
