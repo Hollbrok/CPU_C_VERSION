8a0c3170eff07b5aef8c7b5111d44828722e087f
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2021-02-08 15:16:03 +0300

    Update cpu_info.h

--- CPU/cpu_info.h
+++ CPU/cpu_info.h
@@ -6,7 +6,16 @@ const int START_SIZE = 10;
 struct ass_code
 {
     double* data            = nullptr;
-    int     ass_size        = 0;
+    int     max_ass_size    = 0;
+    //int     ass_size        = 0;
+};
+
+struct rix
+{
+    double rax = NAN;
+    double rbx = NAN;
+    double rcx = NAN;
+    double rdx = NAN;
 };
 
 #endif // CPU_INFO_H_INCLUDED
