9841afbf7647945725cd63a0b5d7c3e657a547fe
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2021-02-08 15:14:30 +0300

    Update assem.h

--- Assembler/assem.h
+++ Assembler/assem.h
@@ -1,38 +1,32 @@
-#ifndef ASSEM_H_INCLUDED
-#define ASSEM_H_INCLUDED
+#ifndef ASSEM_INFO_H_INCLUDED
+#define ASSEM_INFO_H_INCLUDED
 
-#include <Txlib.h>
-#include <time.h>
-#include "assem_info.h"
+struct line_t
+{
+    char *line  = nullptr;
+    long length = 0;
+};
 
-#define DOUBLE_T        // that's the type of data
-/*#define DEBUG*/       // if you need to find a bug or fix something
-#include <stack.h>
+struct text_t
+{
+    char* data              = nullptr;
+    line_t* lines           = nullptr;
+    int n_struct            = 0;
+    int length_file         = 0;
+};
 
-#include "Commands.h"
+struct code_t
+{
+    char*   data            = nullptr;
+    int     length          = 0;
+    int     terms           = 0;
+};
 
-inline other_sizes(long length, long n_structs, int file_lines, text_t* text_s, line_t *lines);
+struct ass_code
+{
+    double* data            = nullptr;
+    int     ass_size        = 0;
+};
 
-void text_construct(text_t* text, FILE* onegin);
 
-void get_code(text_t* text_s, code_t* code_s);
-
-void get_ass_code(code_t* code_s, ass_code* ass_s);
-
-void text_destruct(text_t* text);
-
-void code_destruct(code_t* code_s);
-
-void ass_code_destruct(ass_code* ass_s);
-
-void useful_sizes(FILE* text, text_t* text_s, int* file_lines, long* file_lenght);
-
-void printf_text_s(text_t* text_s);
-
-long size_of_file(FILE* text);
-
-char* set_time(struct tm *time);
-
-char* define_date(void);
-
-#endif // ASSEM_H_INCLUDED
+#endif // ASSEM_INFO_H_INCLUDED
