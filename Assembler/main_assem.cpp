265083452028e3c1291d717a19a018d81c6bed21
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2021-02-09 22:48:56 +0300

    Update main_assem.cpp

--- Assembler/main_assem.cpp
+++ Assembler/main_assem.cpp
@@ -17,9 +17,9 @@ int main()//int main(int argc, char *argv[])
     struct ass_code ass_s = {};
     get_ass_code(&code_s, &ass_s); // получает ассемблерный код из текста
 
-    text_destruct(&text_s);     // деконструктор текста
-    code_destruct(&code_s);     // деконструктор преобразованного текста
-    ass_code_destruct(&ass_s);  // деконструктор ассемблерного текста
+    text_destruct(&text_s);        // деконструктор текста
+    code_destruct(&code_s);        // деконструктор преобразованного текста
+    ass_code_destruct(&ass_s);     // деконструктор ассемблерного текста
 
     printf("DONE!!\n");
 
