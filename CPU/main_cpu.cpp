835da3c9dccc902cb9966fe31d4dfd3f8e52a618
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2021-02-09 22:46:59 +0300

    Update main_cpu.cpp

--- CPU/main_cpu.cpp
+++ CPU/main_cpu.cpp
@@ -9,7 +9,7 @@ int main()
     FILE* text = fopen("[!]assembler_code.txt", "r");
     FILE* res  = fopen("[!]log_stack.txt", "ab");
 
-    fprintf(res, "\tДата log_stack'a : %s (чч/мм/гг)\n\n", define_date());
+    fprintf(res, "\tDate of the log_stack : %s (dd/mm/yy)\n\n", define_date());
     fclose(res);
 
     char* name = nullptr;
@@ -19,13 +19,11 @@ int main()
     struct ass_code ass_s = {};
     make_ass_s(text, &ass_s);
 
-
-    CPU(&ass_s, &Stack);     //(&ass_s, &Stack, &reg_s); //with registers
+    CPU(&ass_s, &Stack);
 
     //print_for_user(&Stack);  // information for user
-    stack_dump(&Stack);      // information for me
+    stack_dump(&Stack);        // information for me
     printf("DONE!!\n");
-    //getch();
 
     ass_code_destruct(&ass_s);
 
