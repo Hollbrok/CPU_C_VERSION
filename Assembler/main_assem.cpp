78f4b13a861ae3508670c0ccd2f0f6a612b6e443
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2021-02-21 01:52:34 +0300

    Update main_assem.cpp

--- Assembler/main_assem.cpp
+++ Assembler/main_assem.cpp
@@ -3,16 +3,14 @@
 int main()//int main(int argc, char *argv[])
 {
     printf("Assembling in progress..\n");
-    setlocale(LC_ALL, "russian");
 
-    text_t text_s    = {};          //структура текста
-
-    FILE* text = fopen("user_code[for user].txt", "r");
-
+    FILE* text    = fopen("user_code[for user].txt", "r");
+    assert(text);
+    text_t text_s = {};          //структура текста
     text_construct(&text_s, text);
 
     struct code_t code_s = {};
-    get_code(&text_s, &code_s);     // преобразует весь код в строчку (просто пока что для удобства)
+    get_code(&text_s, &code_s);     // вычисляет кол-во команд + удобнее обрабатывать код
 
     struct ass_code ass_s = {};
     get_ass_code(&code_s, &ass_s);  // получает ассемблерный код из текста
