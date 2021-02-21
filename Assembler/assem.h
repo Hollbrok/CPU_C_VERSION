e6712a7bd3f9813cc685e4297e7f77a74f59c2e7
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2021-02-22 00:00:36 +0300

    Update assem.h

--- Assembler/assem.h
+++ Assembler/assem.h
@@ -13,7 +13,11 @@
 
 void text_construct(text_t* text, FILE* onegin);
 
-void get_code(text_t* text_s, code_t* code_s);
+void useful_sizes(FILE* text, text_t* text_s, int* file_lines, long* file_lenght);
+
+void code_construct(text_t* text_s, code_t* code_s);
+
+void print_code_buffer(code_t* code_s); // печатает в файл code_struct буфер структуры code (для просмотра как робят комментарии)
 
 void get_ass_code(code_t* code_s, ass_code* ass_s);
 
@@ -23,15 +27,11 @@ void code_destruct(code_t* code_s);
 
 void ass_code_destruct(ass_code* ass_s);
 
-void useful_sizes(FILE* text, text_t* text_s, int* file_lines, long* file_lenght);
-
-void printf_text_s(text_t* text_s);
+void printf_text_s(text_t* text_s); // печатает в файл string_text текст без комментариев в виде строки
 
 char* set_time(struct tm *time);
 
 char* define_date(void);
-
-inline make_full_text_struct(long length, long n_structs, int file_lines, text_t* text_s, line_t *lines);
 
 long size_of_file(FILE* text);
 #endif // ASSEM_H_INCLUDED
