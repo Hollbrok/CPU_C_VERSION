cbf384d5ef03d9229673a981a4b881a55d57e443
Talashkevich Daniil <70323332+Hollbrok@users.noreply.github.com>
2021-02-22 00:01:24 +0300

    Update assem_info.h

--- Assembler/assem_info.h
+++ Assembler/assem_info.h
@@ -5,23 +5,23 @@ constexpr int size_time     = 40;
 
 struct line_t
 {
-    char *line  = nullptr;
-    long length = 0;
+    char *line  = nullptr;              // указатель на буфер строки
+    long length = 0;                    // длина строки
 };
 
 struct text_t
 {
-    char* data              = nullptr;
-    line_t* lines           = nullptr;
-    int n_struct            = 0;
-    int length_file         = 0;
+    char* data              = nullptr;  // указатель на буфер
+    line_t* lines           = nullptr;  // указатель на структуру строки (то есть массив структур строк)
+    int n_struct            = 0;        // кол-во непустых строк
+    int length_file         = 0;        // длина файла, то есть размер буфера
 };
 
 struct code_t
 {
-    char*   data            = nullptr;
-    int     length          = 0;
-    int     terms           = 0;
+    char*   data            = nullptr;  // указатель на буфер кода
+    int     length          = 0;        // длина буфера
+    int     terms           = 0;        // кол-во лексем
 };
 
 struct ass_code
